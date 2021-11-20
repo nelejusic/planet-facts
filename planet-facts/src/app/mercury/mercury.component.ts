54e49cb914565cc851efd60fa5662c55900fd382
nelejusic <72813469+nelethemamba998@users.noreply.github.com>
2021-11-20 21:01:29 +0100

    alot of changes

--- planet-facts/src/app/mercury/mercury.component.ts
+++ planet-facts/src/app/mercury/mercury.component.ts
@@ -1,5 +1,5 @@
+import { PlanetService } from './../planet.service';
 import { Component, OnInit } from '@angular/core';
-import { PlanetService } from '../planet.service';
 @Component({
   selector: 'app-mercury',
   templateUrl: './mercury.component.html',
@@ -7,6 +7,11 @@ import { PlanetService } from '../planet.service';
 })
 export class MercuryComponent implements OnInit {
   constructor(private service:PlanetService) { }
+  internal = 'internal'
+  backgroundImg = '../../../utills/assets/planet-mercury.svg'
+  imgStructure;
+  imgOverview;
+  imgGeology
   data=[]
   planetInfo
   source='Source: Wikipedia'
@@ -14,21 +19,24 @@ export class MercuryComponent implements OnInit {
   radius
   revolution
   rotation
-  basicInfo = []
+  basicInfo = [];
+  structureContent;
+  geologyContent
   ngOnInit(): void {
     this.service.getJSON().subscribe(res=>{
       res.forEach(planet => {
         
         if(planet.name ==='Mercury'){
+          this.structureContent= planet.structure.content;
+          this.geologyContent = planet.geology.content
+          console.log(planet.images.planet)
           this.data.push(planet)
           this.planetInfo = planet.content
           this.radius= planet.radius; this.revolution= planet.revolution; this.rotation=planet.rotation
-          console.log(this.basicInfo)
 
         }
       });
       this.data.forEach(planet=>{
-        console.log(this.data)
         this.planetInfo = planet.overview
         this.link= this.planetInfo.source
         this.planetInfo = this.planetInfo.content
@@ -36,5 +44,28 @@ export class MercuryComponent implements OnInit {
 
     })
   }
+  geologyView;
+  onViewChangeInternal(){
+    document.getElementById('planet-container').classList.remove('planet-container')
+    document.getElementById('planet-container').classList.add('planet-container-internal')
+    this.planetInfo = this.structureContent
+    this.geologyView = false;
 
+  }
+  onViewChangeOverview(){
+    document.getElementById('planet-container').classList.remove('planet-container-internal')
+    document.getElementById('planet-container').classList.add('planet-container')
+    this.geologyView = false;
+
+
+    this.ngOnInit()
+  } 
+   onViewChangeGeology(){
+     this.geologyView = true;
+  
+
+
+
+    this.planetInfo = this.geologyContent;
+  }
 }
--- planet-facts/src/app/neptune/neptune.component.ts
+++ planet-facts/src/app/neptune/neptune.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlanetService } from '../planet.service';
 
 @Component({
   selector: 'app-neptune',
@@ -7,9 +8,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NeptuneComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service:PlanetService) { }
+  internal = 'internal'
+  backgroundImg = '../../../utills/assets/planet-mercury.svg'
+  imgStructure;
+  imgOverview;
+  imgGeology
+  data=[]
+  planetInfo
+  source='Source: Wikipedia'
+  link;
+  radius
+  revolution
+  rotation
+  basicInfo = [];
+  structureContent;
+  geologyContent
   ngOnInit(): void {
-  }
+    this.service.getJSON().subscribe(res=>{
+      res.forEach(planet => {
+        
+        if(planet.name ==='Neptune'){
+          this.structureContent= planet.structure.content;
+          this.geologyContent = planet.geology.content
+          console.log(planet.images.planet)
+          this.data.push(planet)
+          this.planetInfo = planet.content
+          this.radius= planet.radius; this.revolution= planet.revolution; this.rotation=planet.rotation
 
+        }
+      });
+      this.data.forEach(planet=>{
+        this.planetInfo = planet.overview
+        this.link= this.planetInfo.source
+        this.planetInfo = this.planetInfo.content
+      })
+
+    })
+  }
+  geologyView;
+  onViewChangeInternal(){
+    document.getElementById('planet-container').classList.remove('planet-container')
+    document.getElementById('planet-container').classList.add('planet-container-internal')
+    this.planetInfo = this.structureContent
+    this.geologyView = false;
+
+  }
+  onViewChangeOverview(){
+    document.getElementById('planet-container').classList.remove('planet-container-internal')
+    document.getElementById('planet-container').classList.add('planet-container')
+    this.geologyView = false;
+
+
+    this.ngOnInit()
+  } 
+   onViewChangeGeology(){
+     this.geologyView = true;
+  
+
+
+
+    this.planetInfo = this.geologyContent;
+  }
 }
